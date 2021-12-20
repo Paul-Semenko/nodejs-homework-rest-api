0dc1245b30d823ab31c6e639108f2625ffb6771e
Paul-Semenko <paul.semenko.ps@gmail.com>
2021-12-20 20:27:00 +0200

    commit -01-done

---
+++ routes/api/validation.js
@@ -0,0 +1,46 @@
+import Joi from 'joi'
+
+const createSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+    
+})
+const updateSchema = Joi.object({
+    name: Joi.string().optional(),
+    email: Joi.string().email().optional(),
+    phone: Joi.string().optional(),
+}).or('name', 'email', 'phone')
+const idSchema = Joi.object({ id: Joi.string().required(),
+})
+export const validateCreate = async (req, res, next) => {
+    try {
+        const value = await createSchema.validateAsync(req.body)
+    } catch (error) {
+        return res.status(400).json({ message: error.message.replace(/"/g, '') })
+        
+    }
+    next()
+}
+export const validateUpdate = async (req, res, next) => {
+    try {
+        const value = await updateSchema.validateAsync(req.body)
+    } catch (error) {
+        const [{ type }] = err.details
+        if (type === 'object.unknown') {
+            return res.status(400).json({message:err.message})
+        }
+        return res.status(400).json({ message: `fields is missing` })
+        
+    }
+    next()
+}
+export const validateId= async (req, res, next) => {
+    try {
+        const value = await idSchema.validateAsync(req.params)
+    } catch (error) {
+        return res.status(400).json({ message: error.message.replace(/"/g, '') })
+        
+    }
+    next()
+}
